a989316aa6410133141468736b2932ae3f02eec6
cenunix <user55596@protonmail.com>
2026-04-16 04:44:23 -0700

    cleanup

--- modules/hosts/node0/configuration.nix
+++ modules/hosts/node0/configuration.nix
@@ -62,102 +62,6 @@
           };
         };
 
-        #   git = {
-        #     enable = true;
-        #     # package = pkgs.gitAndTools.gitFull;
-        #     ignores = [
-        #       ".cache/"
-        #       ".idea/"
-        #       "*.swp"
-        #       "*.elc"
-        #       ".~lock*"
-        #       "auto-save-list"
-        #       ".direnv/"
-        #       "node_modules"
-        #       "result"
-        #       "result-*"
-        #     ];
-        #
-        #     lfs.enable = true;
-        #
-        #     settings = {
-        #       user.name = "cenunix";
-        #       user.email = "user55596@protonmail.com";
-        #
-        #       init.defaultBranch = "main";
-        #       credential.helper = "oauth";
-        #       delta = {
-        #         enable = true;
-        #         line-numbers = true;
-        #         options.navigate = true;
-        #       };
-        #
-        #       branch.autosetupmerge = "true";
-        #       pull.ff = "only";
-        #       http.postBuffer = "524288000";
-        #       push = {
-        #         default = "current";
-        #         followTags = true;
-        #       };
-        #
-        #       merge = {
-        #         stat = "true";
-        #         conflictstyle = "diff3";
-        #       };
-        #
-        #       core.whitespace = "fix,-indent-with-non-tab,trailing-space,cr-at-eol";
-        #       color.ui = "auto";
-        #
-        #       repack.usedeltabaseoffset = "true";
-        #
-        #       rebase = {
-        #         autoSquash = true;
-        #         autoStash = true;
-        #       };
-        #
-        #       rerere = {
-        #         autoupdate = true;
-        #         enabled = true;
-        #       };
-        #
-        #       url = {
-        #         "https://github.com/".insteadOf = "github:";
-        #         "ssh://git@github.com/".pushInsteadOf = "github:";
-        #         "https://gitlab.com/".insteadOf = "gitlab:";
-        #         "ssh://git@gitlab.com/".pushInsteadOf = "gitlab:";
-        #         "https://aur.archlinux.org/".insteadOf = "aur:";
-        #         "ssh://aur@aur.archlinux.org/".pushInsteadOf = "aur:";
-        #         "https://git.sr.ht/".insteadOf = "srht:";
-        #         "ssh://git@git.sr.ht/".pushInsteadOf = "srht:";
-        #         "https://codeberg.org/".insteadOf = "codeberg:";
-        #         "ssh://git@codeberg.org/".pushInsteadOf = "codeberg:";
-        #       };
-        #
-        #       alias = {
-        #         br = "branch";
-        #         c = "commit -m";
-        #         ca = "commit -am";
-        #         co = "checkout";
-        #         d = "diff";
-        #         df = "!git hist | peco | awk '{print $2}' | xargs -I {} git diff {}^ {}";
-        #         edit-unmerged = "!f() { git ls-files --unmerged | cut -f2 | sort -u ; }; vim `f`";
-        #         fuck = "commit --amend -m";
-        #         graph = "log --all --decorate --graph";
-        #         ps = "!git push origin $(git rev-parse --abbrev-ref HEAD)";
-        #         pl = "!git pull origin $(git rev-parse --abbrev-ref HEAD)";
-        #         af = "!git add $(git ls-files -m -o --exclude-standard | fzf -m)";
-        #         st = "status";
-        #         hist = ''
-        #           log --pretty=format:"%Cgreen%h %Creset%cd %Cblue[%cn] %Creset%s%C(yellow)%d%C(reset)" --graph --date=relative --decorate --all
-        #         '';
-        #         llog = ''
-        #           log --graph --name-status --pretty=format:"%C(red)%h %C(reset)(%cd) %C(green)%an %Creset%s %C(yellow)%d%Creset" --date=relative
-        #         '';
-        #       };
-        #     };
-        #   };
-        # };
-
       };
       nix.settings.experimental-features = [
         "nix-command"
@@ -175,11 +79,7 @@
       networking.hostName = "node0"; # Define your hostname.
 
       environment.systemPackages = with pkgs; [
-        git
         thunar
-        firefox
-        zed
-        helix
         vscode
       ];
     };
